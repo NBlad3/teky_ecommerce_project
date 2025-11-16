e2b766dab01b075a82f5a2038e3907a099df655e
NBlad3 <NBFriend123@outlook.com>
2025-11-16 10:01:41 +0100

    first commit

---
+++ ecommerce_ui/src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import ListProfile from "./components/ListProfile"; 
+
+function App() {
+  return (
+    <div className="App">
+      <ListProfile />
+    </div>
+  );
+}
+
+export default App;
